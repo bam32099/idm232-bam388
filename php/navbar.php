e06eaab217e862093e18ca0ee7ec10f20603fbda
Ben Murray <bam388@drexel.edu>
2019-06-13 15:39:09 -0400

    Case Study / Final PHP Update

--- php/navbar.php
+++ php/navbar.php
@@ -1,13 +1,8 @@
-<html>
- <head>
- <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+
     <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="homepage.css">
- </head>
+
  <body>
 
 <nav role="navigation">
